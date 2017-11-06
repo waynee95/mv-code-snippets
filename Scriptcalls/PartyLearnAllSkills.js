f136defff099e503fbdd31635babac98acfcc721
waynee95 <larsericmarquardt@mail.de>
2017-11-07 00:49:41 +0100

    Added PartyLearnAllSkills
    You can run this snippet in an autorun event at the game start, to give each actor in the all skills. For debuggin purposes.

---
+++ Scriptcalls/PartyLearnAllSkills.js
@@ -0,0 +1,12 @@
+/**
+ * You can run this snippet in an autorun event at the game start, 
+ * to give each actor in the all skills. For debuggin purposes.
+ */
+
+$gameParty.members().forEach(function(member) {
+  $dataSkills.forEach(function(skill) {
+    if (skill && skill.name != "") {
+      member.learnSkill(skill.id);
+    }
+  });
+});
