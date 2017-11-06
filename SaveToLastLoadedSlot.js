c3d6be0da101a48af9bbf36d4c6266e4314fa958
waynee95 <larsericmarquardt@mail.de>
2017-11-07 00:29:18 +0100

    Added SaveToLastLoadedSlot
    This snippet adds DataManager.saveToLastLoadedSlot(). You can call that function in an event to save the game to the  last loaded slot.

---
+++ SaveToLastLoadedSlot.js
@@ -0,0 +1,30 @@
+/**
+ * This snippet adds DataManager.saveToLastLoadedSlot().
+ * You can call that function in an event to save the game to the
+ * last loaded slot.
+ */
+
+(function() {
+  DataManager._lastLoadedSlotID = 1;
+  DataManager.loadGameWithoutRescue = function(savefileId) {
+    var globalInfo = this.loadGlobalInfo();
+    if (this.isThisGameFile(savefileId)) {
+      var json = StorageManager.load(savefileId);
+      this.createGameObjects();
+      this.extractSaveContents(JsonEx.parse(json));
+      this._lastAccessedId = savefileId;
+      this._lastLoadedSlotID = savefileId;
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  DataManager.saveToLastLoadedSlot = function() {
+    var slotID = this._lastLoadedSlotID;
+    $gameSystem.onBeforeSave();
+    if (DataManager.saveGame(slotID)) {
+      StorageManager.cleanBackup(slotID);
+    }
+  };
+})();
